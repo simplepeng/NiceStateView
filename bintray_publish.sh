6d347227ad551ef1906abe0ed54e50e09d55fd28
陈鹏 <simple19930611@gmail.com>
2020-01-13 14:42:16 +0800

    update

--- bintray_publish.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "----------------------------------start----------------------------------"
-
-./gradlew clean build bintrayUpload -PbintrayUser=simplepeng -PbintrayKey=e5e52eecf87d7786b59fe828cab0ee8377934ecd -PdryRun=false
-
-echo "----------------------------------finish----------------------------------"
