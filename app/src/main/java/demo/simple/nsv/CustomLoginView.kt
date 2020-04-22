6d38b95024f3ad3dae57b36dd6af7ae5ccc0757d
陈鹏 <simple19930611@gmail.com>
2020-04-22 16:22:44 +0800

    update:AdapterStateView

--- app/src/main/java/demo/simple/nsv/CustomLoginView.kt
+++ app/src/main/java/demo/simple/nsv/CustomLoginView.kt
@@ -1,9 +1,18 @@
 package demo.simple.nsv
 
+import android.view.View
 import me.simple.nsv.IStateView
 
 class CustomLoginView : IStateView() {
     override fun setLayoutRes(): Int {
         return R.layout.layout_login
+    }
+
+    override fun onAttach(view: View) {
+        super.onAttach(view)
+    }
+
+    override fun onDetach(view: View) {
+        super.onDetach(view)
     }
 }
